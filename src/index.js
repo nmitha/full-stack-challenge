c7e7aa04eeff3ace21fc4cbac73c0d3e33055431
Nadeem Mitha <nadeem.mitha@gmail.com>
2019-06-26 11:46:02 -0400

    Test Stickler CI

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 
 import './index.css';
 
+const a='b'; // unused variable to test Stickler CI
+
+const myFun= c => (d) =>{ return true; }; // messy code test
+
+console.log('test');
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
